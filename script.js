a3ecce2b0e1c0bea620860b98312663b6b2959a8
Abhinaba <abhinaba006@gmail.com>
2023-08-13 14:43:47 +0530

    dsihfsdf

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ const playBtn_el = document.getElementById('playBtn');
 let image = document.querySelector('#bulbImg');
 const decorateBtn_el = document.getElementById('decorateBtn');
 const button_el = document.getElementById('cake');
-function run(){
+function run() {
     changeBGColor('#FFD9B7');
     image.style.opacity = 0;
     playBtn_el.style.display = 'block';
 }
-function changeBGColor(color){
+function changeBGColor(color) {
     document.body.style.background = color;
-    
+
 }
 
 //this code plays music. RUNS FINE.
-function playMusic(){
+function playMusic() {
     var song = document.getElementById('music');
     song.play();
     decorateBtn_el.style.display = 'block';
@@ -22,7 +22,7 @@ function playMusic(){
 
 /*flying banner*/
 const bannerImage_el = document.getElementById('bannerImage');
-decorateBtn_el.addEventListener("click",function(){
+decorateBtn_el.addEventListener("click", function () {
     bannerImage_el.style.display = 'block';
     bannerImage_el.classList.add('flying');
     setTimeout(() => {
@@ -30,6 +30,10 @@ decorateBtn_el.addEventListener("click",function(){
         bannerImage_el.classList.add('fixed');
     }, 5000);
     button_el.style.display = 'block';
+    setTimeout(() => {
+        console.log("hi");
+        button_el.disabled = false;
+    }, 3000);
 })
 /*function animateBanner(){
     bannerImage_el.style.display = 'block';
@@ -42,8 +46,17 @@ decorateBtn_el.addEventListener("click",function(){
 */
 /*Birthday Cake*/
 const mycake_el = document.getElementById("cakeSVG");
-button_el.addEventListener("click", function(){
-  mycake_el.style.display = 'block';
+button_el.addEventListener("click", function () {
+    const contentDiv = document.getElementById("contentDiv");
+
+    fetch("temp.html")
+        .then(response => response.text())
+        .then(htmlContent => {
+            contentDiv.innerHTML = htmlContent;
+        })
+        .catch(error => {
+            console.error("Error loading content:", error);
+        });
 })
 /*make the bulb glow pink */
 /*const bulbElement = document.querySelector('.bulb');
